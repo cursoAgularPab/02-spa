868596800062ee8c5cecf57056628838652dd492
pabmargera <get_balzac@hotmail.com>
2022-05-31 21:49:34 -0500

    Agregando routeo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+// import { HomeComponent } from './components/home/home.component';
+
+
+const APP_ROUTES : Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '**', pathMatch: 'full', redirectTo:'home' }
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
